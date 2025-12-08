386e3a1305c68a46e3ae925721623856caa33717
louis <louis@DESKTOP-3VO1QTR>
2025-12-08 15:18:53 +0100

    Add project files.

---
+++ looter-videogame/looter-videogame.cpp
@@ -0,0 +1,12 @@
+﻿// looter-videogame.cpp : Defines the entry point for the application.
+//
+
+#include "looter-videogame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
